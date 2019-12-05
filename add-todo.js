45ffd4ee50c14b1f5e094160829f079a54082054
phenix1229 <paul.garay@codeimmersives.com>
2019-12-04 21:44:29 -0500

    added front end for sort and filter features

--- add-todo.js
+++ add-todo.js
@@ -4,5 +4,13 @@ const addTodo = function(todo) {
   todos.push(todo);
 }
 
-const userInput = document.querySelector('input').value
-document.querySelector('.add-todo').addEventListener('click', () => printTodo(addTodo(userInput)))
+const userInput = document.querySelector('input');
+document.querySelector('.add-todo').addEventListener('click', () => printTodo(userInput.value));
+const sortChoice = document.querySelector('.sort');
+document.querySelector('.sort-todo').addEventListener('click', (sortChoice) => {
+  let currentTodos = [];
+  if (sortChoice.value === 1){
+    todos.forEach(todo => todo[complete] === true ? currentTodos.push(todo) : currentTodos.unshift(todo));
+  }
+  printTodos(currentTodos)
+})
